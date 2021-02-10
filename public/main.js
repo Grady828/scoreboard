634b31bd9a30aa10b703f8e001511a1edb193412
Mike Morris <eight20eight@gmail.com>
2021-02-10 18:42:59 -0500

    got team 1 name update working

--- public/main.js
+++ public/main.js
@@ -1,7 +1,16 @@
+function updateTeamName() {
+  const team1 = document.querySelector('input.Team1NameInput').value
+  document.querySelector('h2.Team1Name').textContent = team1
+  console.log(team1)
+}
+
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  if (document.querySelector('h2.Team1Name')) {
+    document.querySelector('h2.Team1Name').textContent = 'Team 1'
   }
+  document
+    .querySelector('button.update-TeamName1-Button')
+    .addEventListener('click', updateTeamName)
 }
 
 document.addEventListener('DOMContentLoaded', main)
